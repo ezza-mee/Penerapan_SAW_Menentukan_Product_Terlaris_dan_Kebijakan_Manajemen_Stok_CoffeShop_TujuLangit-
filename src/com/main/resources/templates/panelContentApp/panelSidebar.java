bc74c36ca1fcf5d4934d139eb3d89bfa35cdea38
ezza-mee <rezaaryairhamilah@gmail.com>
2025-03-17 23:27:01 +0700

    add-panel-sidebar-dashboard

--- src/com/main/resources/templates/panelContentApp/panelSidebar.java
+++ src/com/main/resources/templates/panelContentApp/panelSidebar.java
@@ -1,5 +1,23 @@
 package com.main.resources.templates.panelContentApp;
 
-public class panelSidebar {
-    
+import javax.swing.*;
+
+import com.partials.*;
+
+public class panelSidebar extends JPanel {
+
+    public JPanel panelSidebar = new JPanel();
+
+    public panelSidebar() {
+        super();
+        setSize(240, 768);
+        setLayout(null);
+
+        panelSidebar.setBackground(color.GREEN);
+        panelSidebar.setBounds(0, 0, 240, 768);
+        panelSidebar.setLayout(null);
+
+        add(panelSidebar);
+    }
+
 }
