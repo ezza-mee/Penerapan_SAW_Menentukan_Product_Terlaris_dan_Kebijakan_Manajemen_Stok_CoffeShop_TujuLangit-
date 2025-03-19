196338fa6857e2ae3f6a847383b41f5c950a43b5
ezza-mee <rezaaryairhamilah@gmail.com>
2025-03-19 22:13:34 +0700

    add-templates-popUp-notification

--- src/com/main/resources/templates/framePopUp/framePopUp.java
+++ src/com/main/resources/templates/framePopUp/framePopUp.java
@@ -1,5 +1,39 @@
 package com.main.resources.templates.framePopUp;
 
-public class framePopUp {
+import javax.swing.*;
+
+import com.partials.*;
+
+public class framePopUp extends JFrame {
+
+    public int mouseX, mouseY;
+
+    public JPanel popUpPanel = new JPanel();
+
+    public framePopUp() {
+        super();
+        setSize(300, 300);
+        setLayout(null);
+        setUndecorated(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        add(popUpPanel);
+
+        popUpPanel.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent event){
+                mouseX = event.getX();
+                mouseY = event.getY();
+            }
+        });
+
+        popUpPanel.addMouseMotionListener(new MouseAdapter(){
+            public void mouseDragged(MouseEvent event){
+                int newX = event.getXOnScreen() - mouseX;
+                int newY = event.getYOnScreen() - mouseY;
+                setLocatiuon(newX, newY);
+            }
+        });
+    }
 
 }
